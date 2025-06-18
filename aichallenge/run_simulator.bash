44e6fb60cf0d5c63b50629ae3b53cba428731a00
taikitanaka3 <taiki.tanaka@tier4.jp>
2025-06-18 17:22:18 +0900

    feat: add headless option

--- aichallenge/run_simulator.bash
+++ aichallenge/run_simulator.bash
@@ -3,15 +3,19 @@ AWSIM_DIRECTORY=/aichallenge/simulator/AWSIM
 
 mode="${1}"
 
+if command -v nvidia-smi &>/dev/null && [[ -e /dev/nvidia0 ]]; then
+    echo "[INFO] NVIDIA GPU detected"
+    opts=()
+else
+    echo "[INFO] No NVIDIA GPU detected → running on headless mode"
+    opts=("-headless")
+fi
+
 case "${mode}" in
 "endless")
-    opts=("--endless")
-    ;;
-"cpu")
-    opts=("-nographics -batchmode -force-vulkan")
+    opts+=(" --endless")
     ;;
 *)
-    opts=("")
     ;;
 esac
 
