1571fb7640b61de325a6e42fc6696e57403b93f8
Masahiro Kubota <norikenpi@gmail.com>
2025-09-10 18:07:23 +0900

    fix: add options
    Signed-off-by: Masahiro Kubota <norikenpi@gmail.com>

--- vehicle/record_rosbag.bash
+++ vehicle/record_rosbag.bash
@@ -56,7 +56,7 @@ TOPICS=(
     "/sensing/camera/image_raw"
 )
 
-ros2 bag record -s mcap "${TOPICS[@]}" &
+ros2 bag record -s mcap "${TOPICS[@]}"  -d 60 -o rosbag/rosbag2_$$(date +%Y_%m_%d_%H_%M_%S) --compression-mode file --compression-format zstd &
 
 # バックグラウンドで実行したプロセスのID (PID) を取得する
 ROSBAG_PID=$!
